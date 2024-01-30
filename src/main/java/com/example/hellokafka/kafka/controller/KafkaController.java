adb4b26973d3f27da44a40ff5f9b19817823d687
Pedro Ferreira <pe.ferreira@outlook.com>
2024-01-30 20:14:11 +0000

    Initial and probably last commit

---
+++ src/main/java/com/example/hellokafka/kafka/controller/KafkaController.java
@@ -0,0 +1,20 @@
+package com.example.hellokafka.kafka.controller;
+
+import com.example.hellokafka.kafka.producer.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    @Autowired
+    private MessageProducer messageProducer;
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam("message") String message) {
+        messageProducer.sendMessage("my-topic", message);
+        return "Message sent: " + message;
+    }
+}
